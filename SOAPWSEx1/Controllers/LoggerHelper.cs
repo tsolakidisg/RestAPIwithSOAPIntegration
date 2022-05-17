a102dd178039f9582043d9bfc9ac8aa7da440cd2
miliadou <E63488@nbg.gr>
2022-05-17 10:33:10 +0300

    Add logging implementation

---
+++ SOAPWSEx1/Controllers/LoggerHelper.cs
@@ -0,0 +1,53 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPIwithSOAPIntegration.Controllers
+{
+    public static class LoggerHelper
+    {
+        public static void HeadTableLogging(Guid guid, string order_type, string order_status)
+        {
+            var loggerH = LogManager.GetLogger("Head_Table");
+
+            LogEventInfo headLogEvent = new LogEventInfo(LogLevel.Info, "Head_Table","");
+            headLogEvent.Properties.Add("GUID", guid);
+            headLogEvent.Properties.Add("OrderType", order_type);
+            headLogEvent.Properties.Add("OrderStatus", order_status);
+
+            loggerH.Info(headLogEvent);
+        }
+
+        public static void DetailsTableLogging(Guid guid, Guid uuid, string order_type, string service, string end_system, string state, object payload, object ex)
+        {
+            var loggerD = LogManager.GetLogger("Details_Table");
+
+            if (state == "Error")
+            {
+                LogEventInfo detailsLogEvent = new LogEventInfo(LogLevel.Error, "Details_Table", null, "", new List<object>().ToArray(), (Exception)ex);
+                detailsLogEvent.Properties.Add("GUID", guid);
+                detailsLogEvent.Properties.Add("OrderType", order_type);
+                detailsLogEvent.Properties.Add("UUID", uuid);
+                detailsLogEvent.Properties.Add("Service", service);
+                detailsLogEvent.Properties.Add("EndSystem", end_system);
+                detailsLogEvent.Properties.Add("State", state);
+                detailsLogEvent.Properties.Add("Payload", "");
+                loggerD.Log(detailsLogEvent);
+            }
+            else 
+            {
+                LogEventInfo detailsLogEvent = new LogEventInfo(LogLevel.Info, "Details_Table", "");
+                detailsLogEvent.Properties.Add("GUID", guid);
+                detailsLogEvent.Properties.Add("OrderType", order_type);
+                detailsLogEvent.Properties.Add("UUID", uuid);
+                detailsLogEvent.Properties.Add("Service", service);
+                detailsLogEvent.Properties.Add("EndSystem", end_system);
+                detailsLogEvent.Properties.Add("State", state);
+                detailsLogEvent.Properties.Add("Payload", payload);
+                loggerD.Info(detailsLogEvent);
+            }                   
+        }
+    }
+}
--- SOAPWSEx1/Controllers/OrdersController.cs
+++ SOAPWSEx1/Controllers/OrdersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using NLog;
 using RestAPIwithSOAPIntegration.Models;
 using System;
 using System.Collections.Generic;
@@ -16,25 +18,41 @@ namespace RestAPIwithSOAPIntegration.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class OrdersController : ControllerBase
-    {
-
+    {      
         // GET api/<OrdersController>/5
         [HttpGet("{id}")]
         public async Task<string> Get(int id)
         {
-            // Create service object to connect in the SOAP WS using WSDL
-            OrderService.SoapDemoSoapClient service = new OrderService.SoapDemoSoapClient(OrderService.SoapDemoSoapClient.EndpointConfiguration.SoapDemoSoap, "https://localhost:44349/SoapDemo.asmx?WSDL");
-
-            //Create XML element to receive the response of the SOAP WS
-            XmlElement element = await service.GetOrderStatusAsync(id);
-            XmlNodeReader nodeReader = new XmlNodeReader(element);
-            // Create a DataSet Object to store the result XML
-            DataSet dsOrder = new DataSet();
-            dsOrder.ReadXml(nodeReader, XmlReadMode.Auto);
+            var guid = Guid.NewGuid();
+            var coreApiUuid = Guid.NewGuid();
+            var soapUuid = Guid.NewGuid();
             // Create a JSON string to transform the XML object into JSON
             string JSONString = string.Empty;
-            JSONString = JsonConvert.SerializeObject(dsOrder.Tables[0]);
-            JSONString = JSONString[1..^1]; // Remove the table notation => [ ] (first and last character of the string)
+
+            LoggerHelper.HeadTableLogging(guid, "New Order", "New");
+            LoggerHelper.DetailsTableLogging(guid, coreApiUuid, "New Order", "Orders/id", "Core API", "Initial", "{'Id':" + id + "}", new object() { });
+            // Create service object to connect in the SOAP WS using WSDL
+            LoggerHelper.DetailsTableLogging(guid, soapUuid, "New Order", "SoapDemo.asmx?op=GetOrderStatus", "SOAP_WS", "Sent", "{'Id':" + id + "}", new object() { });
+            try
+            {
+                OrderService.SoapDemoSoapClient service = new OrderService.SoapDemoSoapClient(OrderService.SoapDemoSoapClient.EndpointConfiguration.SoapDemoSoap, "https://localhost/SoapDemo/SoapDemo.asmx?WSDL");
+                //Create XML element to receive the response of the SOAP WS
+                XmlElement element = await service.GetOrderStatusAsync(id);
+                XmlNodeReader nodeReader = new XmlNodeReader(element);
+                // Create a DataSet Object to store the result XML
+                DataSet dsOrder = new DataSet();
+                dsOrder.ReadXml(nodeReader, XmlReadMode.Auto);
+                
+                JSONString = JsonConvert.SerializeObject(dsOrder.Tables[0]);
+                JSONString = JSONString[1..^1]; // Remove the table notation => [ ] (first and last character of the string)
+
+                LoggerHelper.DetailsTableLogging(guid, soapUuid, "New Order", "SoapDemo.asmx?op=GetOrderStatus", "Core API", "Received", JSONString, new object() { });
+                LoggerHelper.DetailsTableLogging(guid, coreApiUuid, "New Order", "Orders/id", "SAP", "Completed", JSONString, new object() { });
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.DetailsTableLogging(guid, soapUuid, "New Order", "SoapDemo.asmx?op=GetOrderStatus", "Core API", "Error", new object() { }, ex);
+            }            
 
             return JSONString;
         }
